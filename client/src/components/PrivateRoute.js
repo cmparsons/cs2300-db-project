c3e506293e68f68abc63095068db27e83ff5c519
cmparsons <christianp130@gmail.com>
2018-04-26 13:31:04 -0500

    Add credit for PrivateRoute component

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 
+/**
+ * Taken and modified from React Router docs:
+ * https://reacttraining.com/react-router/web/example/auth-workflow
+ *
+ * If the user is authenticated and exists, render the desired component.
+ * Otherwise, redirect to homepage.
+ */
 const PrivateRoute = ({
   component: Component,
   authStore: { isAuthenticated },
